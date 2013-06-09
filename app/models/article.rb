e1b9fe29b50e67d0a9544d11cac73bb7e0cd0a8f
SergiuR <rmagnum2002@gmail.com>
2013-06-09 13:48:02 +0300

    Update article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -34,7 +34,7 @@ class Article < ActiveRecord::Base
   # end
 
   def self.in_edition(name)
-    Edition.find_by_name!(name.to_s).articles
+    Edition.where('name = ?', name).articles
   end
 
   # methods to display authors and links to their articles
