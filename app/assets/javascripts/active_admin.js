87bde1ac4b2d1d9b5366d13ab34d0e28025b3dff
Rosca Sergiu <rmagnum2002@gmail.com>
2015-03-23 02:05:26 +0200

    first commit to redesign, added required assets

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -1,1 +1,7 @@
 //= require active_admin/base
+//= require jquery
+//= require jquery_ujs
+//= require chosen-jquery
+//= require jquery.tokeninput
+//= require ckeditor_fix
+//= require ckeditor/init
