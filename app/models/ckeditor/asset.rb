380364579af27c1321a0ed0f17a89980123b34e5
Sergiu Rosca <rmagnum2002@gmail.com>
2018-09-17 23:51:50 +0300

    attempt tp fix ckeditor on production

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,7 +1,9 @@
 class Ckeditor::Asset < ActiveRecord::Base
+  include ActiveRecord
   include Ckeditor::Orm::ActiveRecord::AssetBase
-              
+  include Ckeditor::Backend::CarrierWave
+
   delegate :url, :current_path, :size, :content_type, :filename, :to => :data
-  
+
   validates_presence_of :data
 end
